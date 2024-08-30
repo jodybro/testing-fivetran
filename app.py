169062c43a1dfe39191713062e0dfb9b54b868f9
jodybro <ajodasattaur@gmail.com>
2024-08-30 01:21:25 -0400

    working db validation and sql init query generation

--- app.py
+++ app.py
@@ -5,7 +5,30 @@ import os
 
 from src.testing_fivetran import args
 from src.testing_fivetran import parse_http
+from src.testing_fivetran import db
+
 
 cli_args = args.cli_args()
 http_response = parse_http.parse_http().json()
-print(json.dumps(http_response, indent=4))
+
+# Example usage
+json_str = """
+{
+
+    "id": 1,
+    "name": "John Doe",
+    "age": 30,
+    "is_active": true,
+    "height": 1.75,
+    "address": {
+        "street": "123 Main St",
+        "city": "Anytown"
+
+    },
+    "hobbies": ["reading", "swimming"]
+}
+"""
+
+
+json_data = json.loads(json_str)
+table_name = "users"
--- src/testing_fivetran/db.py
+++ src/testing_fivetran/db.py
@@ -1,16 +1,59 @@
 #!/usr/bin/env python3
 
 import os
-import sys
 import sqlite3
+import json
+from typing import Dict, Any
+
+DEFAULT_DB_FILE = os.getcwd() + "/out.db"
 
 
-def ensure_db_exists(db_file: str = "out.db"):
+def ensure_db_exists(db_file: str = DEFAULT_DB_FILE):
+    print(db_file)
     if not os.path.exists(db_file):
-        with open(db_file, "w"):
-            pass
-        conn = sqlite3.connect(db_file)
-        c = conn.cursor()
-        c.execute("CREATE TABLE users (id INTEGER PRIMARY KEY, name TEXT, email TEXT)")
-        conn.commit()
-        conn.close()
+        with open(db_file, "w") as f:
+            f.write("")
+            f.close()
+    else:
+        pass
+
+
+def connect_db(db_file: str = DEFAULT_DB_FILE):
+    return sqlite3.connect(db_file)
+
+
+def json_to_sqlite_type(value: Any) -> str:
+    if isinstance(value, bool):
+        return "INTEGER"  # SQLite doesn't have a boolean type, so we use INTEGER
+    elif isinstance(value, int):
+        return "INTEGER"
+    elif isinstance(value, float):
+        return "REAL"
+    elif isinstance(value, (str, list, dict)):
+        return "TEXT"  # We'll store complex types as JSON text
+    else:
+        return "TEXT"
+
+
+def generate_create_table_sql(data: Dict[str, Any]) -> str:
+    columns = []
+    for k, v in data.items():
+        column_type = json_to_sqlite_type(v)
+        columns.append(f"{k} {column_type}")
+    columns_sql = ", \n".join(columns)
+    return f"CREATE TABLE data ({columns_sql})"
+
+
+def insert_data(data: Dict[str, Any], table_name: str, db_file: str = DEFAULT_DB_FILE):
+    ensure_db_exists()
+    conn = connect_db(db_file)
+    c = conn.cursor()
+    c.execute(generate_create_table_sql(data))
+    conn.commit()
+    conn.close()
+    conn = connect_db(db_file)
+    c = conn.cursor()
+    c.execute(f"INSERT INTO {table_name} VALUES (?)", (json.dumps(data),))
+    conn.commit()
+    conn.close()
+    return True
